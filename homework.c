4db1fe474a0b9dfe02d9d7d0757a9ea0a4c1311e
dwayne <you@example.com>
2015-10-02 00:33:20 +1000

    init

---
+++ homework.c
@@ -0,0 +1,11 @@
+include <stdio.h>
+
+int main(void) {
+
+        printf("programme starting\n");
+	        asm volatile ("wrmsr");
+
+		        printf("programme finished\n");
+			        return 0;
+				}
+
